fa669b2e35cda1c593eb03707f1234464fd3f28c
Paul van der Walt <paul@denknerd.org>
2014-11-26 15:24:35 +0100

    be more precise

--- snd.c
+++ snd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main(){
     /* this generates some nifty sound. */
-    int t;
+    unsigned int t;
     fprintf(stderr, "size of variable = %d bytes\n", sizeof(t));
     for(t=0;      ;t++)
         putchar(t * (((t>>12)|(t>>8)) & (63&(t>>4))));
